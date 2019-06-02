5080564637162a51373bfdef962839e44d0894eb
Humphrey Huang <humphrey@Humphreys-iMac.local>
2019-06-01 21:42:42 -0400

    oh-my-zsh && vscode-mojave-font

---
+++ other-settings.js
@@ -0,0 +1,7 @@
+// defaults write com.microsoft.VSCode CGFontRenderingFontSmoothingDisabled 0
+// defaults write com.microsoft.VSCode.helper CGFontRenderingFontSmoothingDisabled 0
+// defaults write com.microsoft.VSCode.helper.EH CGFontRenderingFontSmoothingDisabled 0
+// defaults write com.microsoft.VSCode.helper.NP CGFontRenderingFontSmoothingDisabled 0
+
+// export PATH=$HOME/bin:/usr/local/bin:$PATH
+// export ZSH=/Users/humphrey/.oh-my-zsh
